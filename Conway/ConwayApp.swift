b877275e44f0eef573bf367462afc9d1e358b629
Vitaly Kostikov <one.kostikov@gmail.com>
2022-06-25 16:47:53 +0200

    Initial Commit

---
+++ Conway/ConwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConwayApp.swift
+//  Conway
+//
+//  Created by Vitaly on 25/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ConwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
